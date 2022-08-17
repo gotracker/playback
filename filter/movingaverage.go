9b6c64cfebeb0cae93683f25e282fa57f09ab899
Jason Crawford <jason.b.crawford@gmail.com>
2022-08-17 13:12:18 -0700

    fix the distribution of the weighted moving avg

--- filter/movingaverage.go
+++ filter/movingaverage.go
@@ -3,16 +3,14 @@ package filter
 import (
 	"math"
 
+	"github.com/gotracker/gomixing/mixing"
 	"github.com/gotracker/gomixing/volume"
 )
 
-type movavgPoint struct {
-	data  volume.Matrix
-	coeff volume.Volume
-}
-
 type MovingAverage struct {
-	points []movavgPoint
+	points   mixing.MixBuffer
+	coeffs   []volume.Volume
+	writePos int
 }
 
 func NewMovingAverage(windowSize int) Filter {
@@ -20,51 +18,65 @@ func NewMovingAverage(windowSize int) Filter {
 		panic("windowSize cannot be 0")
 	}
 	ma := MovingAverage{
-		points: make([]movavgPoint, windowSize),
+		points: make(mixing.MixBuffer, windowSize),
+		coeffs: make([]volume.Volume, windowSize),
 	}
 
-	sigma := float64(windowSize)
+	// sigma = how wide we want our bell to be
+	sigma := float64(windowSize) / (2.0 * math.Pi)
+	// a = our normalizing constant (how tall the bell is)
 	a := 1.0 / (sigma * math.Sqrt(2.0*math.Pi))
+	// mu = the centerpoint of the bell
 	mu := float64(windowSize) / 2.0
-	sigmasq2 := 2 * sigma * sigma
+
+	twoSigmaSq := 2 * sigma * sigma
+	var acc volume.Volume
 	for x := 0; x < windowSize; x++ {
-		xmu := float64(x) - mu
-		fx := a * math.Exp(-(xmu*xmu)/sigmasq2)
-		if fx < 0 {
-			fx = 0
+		xmu := (float64(x) + 0.5) - mu
+		coeff := volume.Volume(a * math.Exp(-(xmu*xmu)/twoSigmaSq))
+		// clamp our value
+		if coeff < 0 {
+			coeff = 0
 		}
-		if fx > 1 {
-			fx = 1
+		if coeff > 1 {
+			coeff = 1
 		}
-		ma.points[x].coeff = volume.Volume(fx)
+		ma.coeffs[x] = coeff
+		acc += coeff
+	}
+	// normalize
+	if acc != 0 {
+		acc = 1.0 / acc
+		for x := 0; x < windowSize; x++ {
+			ma.coeffs[x] *= acc
+		}
 	}
 	return &ma
 }
 
-func (ma *MovingAverage) Clone() Filter {
+func (ma MovingAverage) Clone() Filter {
 	clone := MovingAverage{
-		points: make([]movavgPoint, len(ma.points)),
+		points:   make(mixing.MixBuffer, len(ma.points)),
+		coeffs:   make([]volume.Volume, len(ma.coeffs)),
+		writePos: ma.writePos,
 	}
 	copy(clone.points, ma.points)
+	copy(clone.coeffs, ma.coeffs)
 	return &clone
 }
 
 func (ma *MovingAverage) Filter(dry volume.Matrix) volume.Matrix {
 	var wet volume.Matrix
-	// shift the data down by 1
 	windowLen := len(ma.points)
-	var lastT volume.Volume
-	for i := 1; i < windowLen; i++ {
-		prevPt := &ma.points[i-1]
-		pt := &ma.points[i]
-		prevPt.data = pt.data
-		wet.Accumulate(pt.data.Apply(prevPt.coeff))
-		lastT = pt.coeff
-	}
+	// shift our history
+	ma.writePos = (ma.writePos + 1) % windowLen
 	// now set our dry data into the buffer
-	ma.points[windowLen-1].data = dry
-	// copy our dry data into our wet matrix
-	wet.Accumulate(dry.Apply(lastT))
+	ma.points[ma.writePos] = dry
+	// add up the points and apply the coefficients
+	for i, pt := range ma.points {
+		cpos := (ma.writePos + i) % windowLen
+		wet.Accumulate(pt.Apply(ma.coeffs[cpos]))
+	}
 
 	return wet
 }
